f8f126431a58d41e737dfbf43ae383bad71db359
Dr.Tang <tangzhenyu_2012@163.com>
2018-01-03 14:17:44 +0800

    Create tf_gather.py

---
+++ tf_gather.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+temp = tf.range(0,10)*10 + tf.constant(1,shape=[10])
+temp2 = tf.gather(temp,[1,5,9])
+
+with tf.Session() as sess:
+
+    print sess.run(temp)
+    print sess.run(temp2)
